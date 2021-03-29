174ad2b2d93312aa2f308ab4a684054f47d443a2
Diego Sousa <diegointeriores11@gmail.com>
2021-03-29 19:49:38 -0300

    touches

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,18 +3,18 @@ const User = require('../models/user')
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.header('Authorization').replace('Bearer', '')
-        const decoded = jwt.verify(token, 'thisismynewproject')
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'thisismynewcourse')
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
-        if(!user) {
+        if (!user) {
             throw new Error()
         }
 
-        res.user = user
+        req.user = user
         next()
-    }catch (e) {
-        res.status(401).send(e)
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate.' })
     }
 }
 
--- src/models/user.js
+++ src/models/user.js
@@ -51,36 +51,38 @@ const userSchema = new mongoose.Schema({
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewproject')
-    
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+
     user.tokens = user.tokens.concat({ token })
     await user.save()
+
     return token
 }
 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
 
-    if(!user) {
+    if (!user) {
         throw new Error('Unable to login')
     }
 
     const isMatch = await bcrypt.compare(password, user.password)
-    
-    if(!isMatch) {
+
+    if (!isMatch) {
         throw new Error('Unable to login')
     }
 
     return user
 }
 
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     const user = this
-    
-    if(user.isModified('password')) {
+
+    if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8)
     }
-    
+
     next()
 })
 
