bd7cc7b1d83c3a65d842293d8746442cc1919ae1
Diego Sousa <diegointeriores11@gmail.com>
2021-04-14 19:23:03 -0300

    remove .env variable

--- src/emails/account.js
+++ src/emails/account.js
@@ -6,7 +6,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: process.env.EMAIL_SENDER,
+        from: 'diegointeriores11@gmail.com',
         subject: 'Welcome to the app!',
         text: `We are glad that you are joining in with us, ${name}!`
     })
@@ -15,7 +15,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendDeletedUserEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: process.env.EMAIL_SENDER,
+        from: 'diegointeriores11@gmail.com',
         subject: 'Thank you',
         text: `It makes us sad that you're not satisfied with our application, ${name}. Let us know if there is something we could've done to keep you with us!`
     })
